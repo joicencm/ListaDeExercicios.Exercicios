2cee1964856e1f14a905b819130d3c17985c98c0
joicencm <joicencm@gmail.com>
2025-04-03 21:08:58 -0300

    Implementação Calculo Salario Base x Vendas x Percentual sobre as vendas

---
+++ ListaDeExercicios.Exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ListaDeExercicios.Exercicio05
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o salário total de um vendedor. Deverá ser informado o salário base
+        //e o total de vendas.
+        //A comissão é calculada com um percentual(informado pelo usuário) sobre o total de vendas.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o salario do vendedor");
+            double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a vendas do vendedor");
+            double vendas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o  percentual de comissão sobre o total de vendas (em %");
+            double percentualComissao = Convert.ToDouble(Console.ReadLine());
+
+            double salarioTotal = salarioBase + (percentualComissao / 100) * vendas;
+
+            Console.WriteLine($"O salário total do vendedor é: R${salarioTotal:F2}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
