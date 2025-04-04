015b852139c0c888e7a6d1e09cfd84933d5f8466
joicencm <joicencm@gmail.com>
2025-04-03 21:41:06 -0300

    Implementação calculo da média ponderada de duas provas e Implementação verificar se um número é primo

---
+++ ListaDeExercicios.Exercicio06/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ListaDeExercicios.Exercicio06
+{
+    internal class Program
+    {
+        //Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+        static void Main(string[] args)
+        {
+            #region imput de dados
+            Console.WriteLine("Digite a nota da primeira prova");
+            double primeiraProva = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Digite a nota da segunda prova");
+            double segundaProva = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            // Definindo os pesos para cada prova
+            double pesoPrimeiraProva = 3;
+            double pesoSegundaProva = 7;
+
+            // Calculando a média ponderada
+            double mediaPonderada = (primeiraProva * pesoPrimeiraProva + segundaProva * pesoSegundaProva)
+                                    / (pesoPrimeiraProva + pesoSegundaProva);
+            #endregion
+
+            #region output dos dados
+            Console.WriteLine($"A média ponderada das provas é: {mediaPonderada}");
+            #endregion
+
+            Console.ReadLine();
+
+        }
+    }
+}
