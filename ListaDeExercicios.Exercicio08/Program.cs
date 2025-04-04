379c022409a9bb0bad8ffe5c9ef9bb9e5573e050
joicencm <joicencm@gmail.com>
2025-04-04 19:49:24 -0300

    Implementação Algoritmo com valores A,B,C

---
+++ ListaDeExercicios.Exercicio08/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ListaDeExercicios.Exercicio08
+{
+    internal class Program
+    {
+        //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+        static void Main(string[] args)
+        {
+            #region imput de dados
+            Console.WriteLine("Digite o valor do A");
+            int valorA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do B");
+            int valorB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do C");
+            int valorC = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            int soma = valorA + valorB;
+            bool resultado = soma < valorC;
+            #endregion
+
+            #region output dos dados
+            if (resultado)
+                Console.WriteLine($"Resultado {valorA} + {valorB} = {soma}. É menor que {valorC}");
+            else
+                Console.WriteLine($"Resultado {valorA} + {valorB} = {soma}. Não é menor que {valorC}");
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
