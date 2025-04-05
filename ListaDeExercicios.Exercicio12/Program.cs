0564de9b610384a1520c9b18efa9933cbaa2fc97
joicencm <joicencm@gmail.com>
2025-04-04 22:02:11 -0300

    Implementação Geração de números impares entre 100 e 200

---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        //Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+        static void Main(string[] args)
+        {
+            #region imput de dados
+
+            #endregion
+
+            #region processamento
+            for (int n = 101; n < 200; n += 2)                          
+                Console.WriteLine(n);            
+            #endregion
+
+            #region output dos dados
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
